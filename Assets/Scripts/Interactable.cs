648d9fca6ce5b52c2acb70b516aa717bfaca9242
SmallPeenwow <bowdencamryn@gmail.com>
2022-09-10 15:54:03 +0200

    Interactable Events Added

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    // Add or remove an InteractionEvent component to gameObject
+    public bool useEvents;
     // Displays message to player when looking at an interactable
     public string promptMessage;
+
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
 
     // This function will be called from the player script
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
+
         Interact();
     }
 
