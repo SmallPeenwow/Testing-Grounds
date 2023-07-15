caeb83cf25635c01607ce479a918d49ef6c506ec
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-15 10:56:45 +0200

    Testing Enemy Attack State Start

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,6 +12,10 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private string currentState; // For Debugging purposes
     public Path path;
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float fieldofView = 85f;
+    public float eyeHeight;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +23,43 @@ public class Enemy : MonoBehaviour
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialise();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CanSeePlayer();
+    }
+
+    public bool CanSeePlayer()
+    {
+        if (player != null)
+        {
+            // Is the player close enough to be seen?
+            if (Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float anglePlayer = Vector3.Angle(targetDirection, transform.forward);
+
+                if (anglePlayer >= -fieldofView && anglePlayer <= fieldofView)
+                {
+                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+
+                    if (Physics.Raycast(ray, out hitInfo, sightDistance))
+                    {
+                        if (hitInfo.transform.gameObject == player)
+                        {
+                            Debug.DrawRay(ray.origin, ray.direction * sightDistance);   
+                            return true;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return false;
     }
 }
